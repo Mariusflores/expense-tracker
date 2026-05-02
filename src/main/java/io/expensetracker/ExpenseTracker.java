2ed5d629496fbd7bc874a7079904ddf56391f6a2
mariusflores <mfloresdal@gmail.com>
2026-05-02 16:55:08 +0200

    updated structure

--- src/main/java/io/expensetracker/ExpenseTracker.java
+++ src/main/java/io/expensetracker/ExpenseTracker.java
@@ -1,6 +1,6 @@
-package org.example;
+package io.expensetracker;
 
-public class Example {
+public class ExpenseTracker {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
